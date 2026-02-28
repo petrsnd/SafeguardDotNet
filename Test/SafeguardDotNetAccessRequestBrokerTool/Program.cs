6d3d82884e28c1123f5dbd70482f4ec9c2def1b9
petrsnd <petrsnd@gmail.com>
2026-02-27 18:05:18 -0700

    Fix class naming issue in analyzer

--- Test/SafeguardDotNetAccessRequestBrokerTool/Program.cs
+++ Test/SafeguardDotNetAccessRequestBrokerTool/Program.cs
@@ -199,14 +199,3 @@ void HandleParseError(IEnumerable<Error> errors)
 Parser.Default.ParseArguments<ToolOptions>(args)
 .WithParsed(Execute)
 .WithNotParsed(HandleParseError);
-
-namespace SafeguardDotNetAccessRequestBrokerTool
-{
-    internal static class StringEnhancements
-    {
-        public static bool IsNumeric(this string str)
-        {
-            return int.TryParse(str, out _);
-        }
-    }
-}
