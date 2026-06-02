5dcbcdbd8ce0d3d7469a06d69dad5d1895eabfb0
petrsnd <petrsnd@gmail.com>
2026-06-02 14:38:00 -0600

    Add source-generated SafeguardJsonContext and LoginResponse DTO

---
+++ SafeguardDotNet/Serialization/SafeguardJsonContext.cs
@@ -0,0 +1,19 @@
+// Copyright (c) One Identity LLC. All rights reserved.
+
+namespace OneIdentity.SafeguardDotNet.Serialization;
+
+using System.Text.Json.Serialization;
+
+using OneIdentity.SafeguardDotNet.A2A;
+
+/// <summary>
+/// Source-generated JSON serializer context for all types that require serialization in the SDK.
+/// Guarantees AOT-compatible, reflection-free serialization at compile time.
+/// </summary>
+[JsonSerializable(typeof(BrokeredAccessRequest))]
+[JsonSerializable(typeof(A2ARetrievableAccount))]
+[JsonSerializable(typeof(ApiKeySecret))]
+[JsonSerializable(typeof(LoginResponse))]
+internal partial class SafeguardJsonContext : JsonSerializerContext
+{
+}
