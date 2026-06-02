7152f62b54082d8062016d2c993f5c490aeb8e71
petrsnd <petrsnd@gmail.com>
2026-06-01 19:40:04 -0600

    Remove old plan and impl async browser login

--- SafeguardDotNet.BrowserLogin/AuthorizationCodeExtractor.cs
+++ SafeguardDotNet.BrowserLogin/AuthorizationCodeExtractor.cs
@@ -2,11 +2,13 @@
 
 namespace OneIdentity.SafeguardDotNet.BrowserLogin;
 
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 
 public class AuthorizationCodeExtractor
@@ -16,6 +18,80 @@ public class AuthorizationCodeExtractor
     }
 
     public string AuthorizationCode { get; set; }
+
+    /// <summary>
+    /// Listens for an OAuth authorization code callback on the specified port (async).
+    /// Returns the authorization code extracted from the browser redirect.
+    /// </summary>
+    /// <remarks>
+    /// WARNING: This method blocks indefinitely until the browser callback is received.
+    /// If no <paramref name="cancellationToken"/> is provided, the call will never return
+    /// if the user does not complete authentication. Always provide a cancellation token
+    /// with a timeout or use <see cref="Safeguard.AgentBasedLoginUtils.CreateConsoleCancellationToken"/>
+    /// to enable Ctrl+C cancellation.
+    /// </remarks>
+    /// <param name="port">Local TCP port to listen on for the OAuth callback.</param>
+    /// <param name="cancellationToken">Cancellation token to abort the listener.</param>
+    /// <returns>The OAuth authorization code from the browser redirect.</returns>
+    /// <exception cref="OperationCanceledException">Thrown when cancellation is requested.</exception>
+    /// <exception cref="SafeguardDotNetException">Thrown when the redirect is malformed or missing.</exception>
+    public static async Task<string> ListenAsync(int port, CancellationToken cancellationToken)
+    {
+        var tcpListener = new TcpListener(IPAddress.Loopback, port);
+        tcpListener.Start();
+
+        try
+        {
+            TcpClient tcpClient;
+            using (cancellationToken.Register(tcpListener.Stop))
+            {
+                try
+                {
+                    tcpClient = await tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex) when ((ex is ObjectDisposedException || ex is SocketException)
+                    && cancellationToken.IsCancellationRequested)
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+            }
+
+            using (tcpClient)
+            {
+                using var networkStream = tcpClient.GetStream();
+                var readBuffer = new byte[1024];
+                var sb = new StringBuilder();
+                do
+                {
+                    var numberOfBytesRead = await networkStream.ReadAsync(readBuffer, 0, readBuffer.Length, cancellationToken)
+                        .ConfigureAwait(false);
+                    var s = Encoding.ASCII.GetString(readBuffer, 0, numberOfBytesRead);
+                    sb.Append(s);
+                }
+                while (networkStream.DataAvailable);
+
+                var fullResponse =
+                    "HTTP/1.1 200 OK\r\n\r\n<html><head><title>Authentication Complete</title></head><body><h2>Authentication complete.</h2>" +
+                    "<p>You can return to your application.</p><p>Feel free to close this browser tab.</p></body></html>\r\n";
+                var response = Encoding.ASCII.GetBytes(fullResponse);
+                await networkStream.WriteAsync(response, 0, response.Length, cancellationToken).ConfigureAwait(false);
+                await networkStream.FlushAsync(cancellationToken).ConfigureAwait(false);
+
+                var httpRequest = sb.ToString();
+                var authCode = HttpUtility.ParseQueryString(ExtractUriFromHttpRequest(httpRequest)).Get("oauth");
+                if (string.IsNullOrEmpty(authCode))
+                {
+                    throw new SafeguardDotNetException("OAuth callback did not contain an authorization code.");
+                }
+
+                return authCode;
+            }
+        }
+        finally
+        {
+            tcpListener.Stop();
+        }
+    }
 
     public void Listen(int port, CancellationToken cancellationToken)
     {
